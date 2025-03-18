5cdeca74618f7441bc678b8a12a32afff38448ce
SenDoan <mariathanhsen@gmail.com>
2025-03-18 16:39:48 -0500

    admin page

--- frontend/src/components/Admin/PropertiesManagement.jsx
+++ frontend/src/components/Admin/PropertiesManagement.jsx
@@ -1,7 +1,6 @@
-
-    import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchOwnersSuccess, fetchPropertiesSuccess } from '../../store/admin/adminSlice';
+import { fetchPropertiesSuccess } from '../../store/admin/adminSlice';
 
 const listProperties = [
   {
@@ -33,37 +32,32 @@ const listProperties = [
   }
 ]
 
-const listOwners = [
-  {
-    id: 1,
-    name: "Sen",
-    email: "sen@gmail.com",
-    status: "Active",
-  },
-  {
-    id: 2,
-    name: "Sen 2",
-    email: "sen@gmail.com",
-    status: "Active",
-  },
-  {
-    id: 3,
-    name: "Sen 3",
-    email: "sen@gmail.com",
-    status: "Active",
-  }
-]
-
 const PropertiesManagement = () => {
     const [tab, setTab] = useState(0);
     const dispatch = useDispatch();
     const {properties} = useSelector((state) => state.admin);
+    const [searchText, setSearchText] = useState("");
+    const renderBadgeClass = (status) => {
+        return status === 'Approval' ? "badge-active" : "badge-deactive";
+    };
 
   useEffect(() => {
     // fetch data
-    dispatch(fetchOwnersSuccess({data: listOwners}));
     dispatch(fetchPropertiesSuccess({data: listProperties}));
-  }, []);
+  }, [dispatch, tab]);
+
+  const onHandleActiveAndDeactive = (id) => {
+      const updatedOwners = properties.map((item) => 
+          item.id === id ? { ...item, status: item.status === "Pending" ? "Approval" : "Pending" } : item
+      );
+  
+      dispatch(fetchPropertiesSuccess({ data: updatedOwners }));
+  };
+  
+    const handleSearchProperty = () => {
+      // fetch API
+      setSearchText("");
+    }
 
   return (
     <div className="flex flex-col gap-4 w-full">
@@ -72,43 +66,58 @@ const PropertiesManagement = () => {
       {/* tabs */}
       <div className='flex flex-row gap-0 items-center justify-start border-b border-gray-200 tabs'>
         <div className={`${tab === 0 ? 'active' : ''} tab-item`} onClick={() => setTab(0)}>All</div>
-        <div className={`${tab === 1 ? 'active' : ''} tab-item`} onClick={() => setTab(1)}>Rented</div>
-        <div className={`${tab === 2 ? 'active' : ''} tab-item`} onClick={() => setTab(2)}>Sold</div>
-        <div className={`${tab === 3 ? 'active' : ''} tab-item`} onClick={() => setTab(3)}>New</div>
+        <div className={`${tab === 1 ? 'active' : ''} tab-item`} onClick={() => setTab(1)}>Active</div>
+      </div>
+
+      {/* Search item */}
+      <div className='flex flex-row justify-end w-full items-stretch'>
+        <input value={searchText} onChange={(e) => setSearchText(e.target.value)} type="text" className='border border-gray-200 p-2 rounded-l-md outline-0' />
+        <button onClick={handleSearchProperty} className='search-button'>Search</button>
       </div>
 
       {/* table */}
       <div className='flex flex-row gap-2 w-full'>
-        <table className={`${tab === 0 ? '' : 'hidden'} table`}>
+        <table className="border-collapse table-fixed w-full">
           <thead>
             <tr>
-              <th><input type="checkbox" name="check_all" /></th>
+              {/* <th><input type="checkbox" name="check_all" /></th> */}
               <th>Id</th>
-              <th>Name</th>
-              <th>Email</th>
+              <th>Property</th>
+              <th>Owner</th>
               <th>Status</th>
+              <th>Actions</th>
             </tr>
           </thead>
 
-          <tbody>
-            
-          </tbody>
-        </table>
-
-        <table className={`${tab === 1 ? '' : 'hidden'}`}>
-          <thead>
-            <tr>
-              <th><input type="checkbox" name="check_all" /></th>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            
-          </tbody>
+            <tbody>
+                {properties && properties.length > 0 ? (
+                    <>
+                    {
+                        properties.map((item, key) => (
+                            <tr key={key}>
+                                {/* <td><input type='checkbox' name={item.id}/></td> */}
+                                <td>{item.id}</td>
+                                <td>{item.property}</td>
+                                <td>{item?.owner?.name}</td>
+                                <td><span className={`badge ${renderBadgeClass(item.status)}`}>{item.status}</span></td>
+                                <td>
+                                    <button 
+                                        onClick={() => onHandleActiveAndDeactive(item.id)} 
+                                        className={`${item.status !== 'Pending' ? 'hidden' : ''} text-sky-600 bg-slate-100 font-bold text-sm px-2 cursor-pointer`}
+                                    >
+                                        Approval
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                    </>
+                ) : (
+                <tr>
+                    <td colSpan="6" className="text-center py-4">No property found.</td>
+                </tr>
+            )}
+            </tbody>
         </table>
       </div>
     </div>
