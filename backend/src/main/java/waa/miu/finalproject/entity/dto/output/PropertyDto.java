a52eba2b7b8e7c8c5d5c8a6577a9a0f2b1d2b695
nhienlq <quangnhienxstkk26@gmail.com>
2025-03-19 11:57:00 -0500

    update property and property dto

--- backend/src/main/java/waa/miu/finalproject/entity/dto/output/PropertyDto.java
+++ backend/src/main/java/waa/miu/finalproject/entity/dto/output/PropertyDto.java
@@ -1,6 +1,8 @@
 package waa.miu.finalproject.entity.dto.output;
 
 import lombok.Data;
+import waa.miu.finalproject.entity.Address;
+import waa.miu.finalproject.entity.dto.UserDto;
 import waa.miu.finalproject.enums.PropertyStatusEnum;
 import waa.miu.finalproject.enums.PropertyTypeEnum;
 
@@ -18,4 +20,7 @@ public class PropertyDto {
     private double view;
     private double save;
     private int yearBuilt;
+    private String houseType;
+    private Address address;
+    private UserDto user;
 }
