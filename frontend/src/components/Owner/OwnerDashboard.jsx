59164498d24eed841ead32cc751d309a584de15a
Devesh Mittal <d.mittal@miu.edu>
2025-03-20 16:18:25 -0500

    Latest changes

--- frontend/src/components/Owner/OwnerDashboard.jsx
+++ frontend/src/components/Owner/OwnerDashboard.jsx
@@ -7,9 +7,9 @@ const OwnerDashboard = () => {
       <h1 className="text-3xl font-semibold mb-6">Owner Dashboard</h1>
       <div className="grid grid-cols-2 gap-4">
         <Link to="/owner" className="bg-blue-500 text-white p-4 rounded-md text-center">Dashboard</Link>
-        <Link to="/owner/property-management" className="bg-green-500 text-white p-4 rounded-md text-center">Manage Properties</Link>
-        <Link to="/owner/offers" className="bg-yellow-500 text-white p-4 rounded-md text-center">View Offers</Link>
-        <Link to="/owner/messages" className="bg-gray-500 text-white p-4 rounded-md text-center">Messages</Link>
+        <Link to="/owner/property-management" className="bg-green-500 p-4 rounded-md text-center">Manage Properties</Link>
+        <Link to="/owner/offers" className="bg-yellow-500 p-4 rounded-md text-center">View Offers</Link>
+        <Link to="/owner/messages" className="bg-gray-500 p-4 rounded-md text-center">Messages</Link>
       </div>
     </div>
   );
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,12 +3,14 @@ import authReducer from "./auth/authSlice";
 import adminReducer from "./admin/adminSlice";
 import homeReducer from "./hompage/homeSlice";
 import customerReducer from "./customer/customerSlice";
+import ownerReducer from "./owner/ownerSlice"
 const store = configureStore({
   reducer: {
     auth: authReducer,
     admin: adminReducer,
     home: homeReducer,
-    customer:customerReducer
+    customer:customerReducer, 
+    owner: ownerReducer
   },
 });
 
